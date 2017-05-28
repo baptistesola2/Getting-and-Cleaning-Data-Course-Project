cb1ac615b4aa7939252596245d9ba15091b97f23
baptistesola2 <sola.baptiste@gmail.com>
2017-05-28 19:18:50 +0200

    Create CourseProject.R

---
+++ CourseProject.R
@@ -0,0 +1,48 @@
+# Download the zip file
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+            "./UCI HAR Dataset.zip")
+
+#unzip the downloaded file 
+unzip("UCI HAR Dataset.zip")
+
+# Read tables from txt files
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",col.names = c("activity_id","activty_name"))
+features <- read.table("./UCI HAR Dataset/features.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",col.names = "subject_id")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",col.names = "subject_id")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt",col.names = features[,2])
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt",col.names = "activity_id")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "activity_id")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt",col.names = features[,2])
+
+# 1 - Merges the training and the test sets to create one data set.
+X_merge <- rbind (X_test,X_train)
+
+# 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
+#Create vector to filter table X_merge
+X_merge <- rbind (X_test,X_train)
+
+
+#3 - Uses descriptive activity names to name the activities in the data set
+# merge y_test and y_train 
+y_merge <-  rbind (y_test,y_train)
+
+# join y_merge with activity_label
+y_merge_label <- merge(y_merge,activity_labels,by = "activity_id")
+
+# merge X_merge_mean_std with y_merge_label
+X_merge_label <- cbind(y_merge_label,X_merge_mean_std)
+
+# 4 - Appropriately labels the data set with descriptive variable names.
+#already done throught read.table 
+names(X_merge_label)
+
+# 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# add collumn for subject
+X_subject <- cbind(subjects_all, X_merge_label)
+# Group by both subject and activity
+grp <- group_by(X_subject, subject_id, activty_name)
+means_per_activity_subject <- summarise_each(grp,funs(mean))
+
+
